0cbadfe3644c1a9b8ea293c09fcb42070ce28931
eakwofie <amankrahkwofie354@gmail.com>
2026-01-16 10:56:55 -0500

    export test

--- backend/question_bank_response_count.py
+++ backend/question_bank_response_count.py
@@ -10,8 +10,10 @@ This script:
 import os
 import django
 
-# Setup Django environment
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+# Setup Django
+import sys
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_core.settings.production')
 django.setup()
 
 from forms.models import QuestionBank, Question
